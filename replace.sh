2189e1b7b2bdd6ae962a382401367c3509c47309
marco <28888207+Marco3jp@users.noreply.github.com>
2018-12-13 15:22:17 +0900

    minify済みのcssを変換するのにスペースが混入していたので修正

--- replace.sh
+++ replace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat app.ts | sed -e "s/font-size:65px;letter-spacing:1px/font-size:\${styleData.subsFontSize};letter-spacing:\${styleData.subsLetterSpacing}/g" -e "s/position:fixed;background-color:rgba(66, 66, 66, 0.5)/position:fixed;background-color:\${styleData.subsBackgroundColor}/g" -e "s/padding:10px 20px/padding:\${styleData.subsPaddingVertical} \${styleData.subsPaddingHorizontal}/g" > app.ts.bak
+cat app.ts | sed -e "s/font-size:65px;letter-spacing:1px/font-size:\${styleData.subsFontSize};letter-spacing:\${styleData.subsLetterSpacing}/g" -e "s/position:fixed;background-color:rgba(66,66,66,.5)/position:fixed;background-color:\${styleData.subsBackgroundColor}/g" -e "s/padding:10px 20px/padding:\${styleData.subsPaddingVertical} \${styleData.subsPaddingHorizontal}/g" > app.ts.bak
 mv app.ts.bak app.ts
